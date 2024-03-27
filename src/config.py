aca2050ce587929d499e647722b4a0c0e995eced
Andrés Ingelmo <aingelmop@gmail.com>
2024-03-27 23:01:04 +0100

    Add import statement for pathlib and update csv_to_sqlite function

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -9,3 +11,7 @@ class PostgresSettings(BaseSettings):
     db_name: str = Field(validation_alias="POSTGRES_DB")
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+POSTGRES_SETTINGS = PostgresSettings()  # type: ignore
+ROOT_DIR = Path(__file__).parents[1]
--- src/ingest_data.py
+++ src/ingest_data.py
@@ -12,10 +12,12 @@ CSV_DIR = DATA_DIR / "csv"
 
 # Function to import a CSV file into a SQLite database
 def csv_to_sqlite(csv_file: Path, table_name: str, engine: Engine):
-    if table_name == "articulo":
+    if table_name == "articles":
         df = pd.read_csv(csv_file, index_col=0)
     else:
-        df = pd.read_csv(csv_file)
+        df = pd.read_csv(csv_file, encoding="latin_1")
+
+    df.to_sql(table_name, engine, if_exists="append", index=False)
     try:
         df.to_sql(table_name, engine, if_exists="append", index=False)
     except IntegrityError:
