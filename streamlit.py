21dcde48f6dfae89e5a72ee6debb201dc8f3cdac
Andrés Ingelmo <aingelmop@gmail.com>
2024-03-28 12:31:22 +0100

    Add Streamlit API for model with cost calculation

---
+++ streamlit.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import requests
+
+import streamlit as st
+
+st.title("API for the model")
+
+available_articles_request = requests.get("http://localhost:8000/articles/all")
+available_articles = available_articles_request.json()
+
+desired_article = st.selectbox("Desired article", available_articles)
+
+article_id_request = requests.get(f"http://localhost:8000/articles/{desired_article}")
+article_id = article_id_request.json()
+
+possibilities_request = requests.get(f"http://localhost:8000/articles/id/{article_id}")
+possibilities = possibilities_request.json()
+
+
+possible_dimensions = {
+    "length": set(
+        dimension["length"] for _, dimension in possibilities["dimensions"].items()
+    ),
+    "width": set(
+        dimension["width"] for _, dimension in possibilities["dimensions"].items()
+    ),
+    "thickness": set(
+        dimension["thickness"] for _, dimension in possibilities["dimensions"].items()
+    ),
+}
+
+
+desired_material = st.radio("Desired material", possibilities["materials"])
+desired_finishing = st.radio("Desired finishing", possibilities["finishings"])
+desired_length = st.selectbox("Desired length", list(possible_dimensions["length"]))
+desired_width = st.selectbox("Desired width", list(possible_dimensions["width"]))
+desired_thickness = st.selectbox(
+    "Desired thickness", list(possible_dimensions["thickness"])
+)
+
+materials_request = requests.get(
+    f"http://localhost:8000/materials/{article_id}/{desired_material}"
+)
+material_cost = materials_request.json()
+st.write(f"Material cost: {material_cost}")
+
+finishing_request = requests.get(
+    f"http://localhost:8000/finishing/{article_id}/{desired_finishing}"
+)
+finishing_cost = finishing_request.json()
+st.write(f"Finishing cost: {finishing_cost}")
+
+dimensions_request = requests.get(
+    f"http://localhost:8000/dimensions/{article_id}/l={desired_length}&w={desired_width}&t={desired_thickness}"
+)
+dimensions_cost = dimensions_request.json()
+st.write(f"Dimensions cost: {dimensions_cost}")
+
+total_cost = material_cost + finishing_cost + dimensions_cost
+st.write(f"Total cost: {total_cost}")
