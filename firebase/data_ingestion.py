2e9637400837ac4022cd3b85abca0ca6171b8810
Andrés Ingelmo <aingelmop@gmail.com>
2024-04-02 13:49:39 +0200

    Update Firebase service account path

--- firebase/data_ingestion.py
+++ firebase/data_ingestion.py
@@ -5,7 +5,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Use a service account
-cred = credentials.Certificate("firebase/sa.json")
+cred = credentials.Certificate("sa.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
