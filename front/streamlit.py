a47a6e6a21ceda3bb1caf53c9693e5b2085d86d1
Andrés Ingelmo <aingelmop@gmail.com>
2024-04-02 15:23:00 +0200

    Add streamlit UI for selecting desired article, material, dimensions, and finishing options

--- front/streamlit.py
+++ front/streamlit.py
@@ -3,38 +3,52 @@ import streamlit as st
 
 API_HOST = st.secrets["api_host"]
 
+
+def format_option(option):
+    if isinstance(option, dict) and "name" in option:
+        return option["name"]
+    else:
+        return str(option)
+
+
 st.title("API for the model")
 
 available_articles = requests.get(f"http://{API_HOST}/articles/all").json()
 
 article_names = [article["name"] for article in available_articles]
 
+desired_article = st.selectbox(
+    "Desired article", available_articles, index=None, format_func=format_option
+)
 
-desired_article_name = st.selectbox("Desired article", article_names)
-desired_article_id = [
-    article["doc_id"]
-    for article in available_articles
-    if article["name"] == desired_article_name
-][0]
+if not desired_article:
+    st.error("Please select an article")
+    st.stop()
 
-article = requests.get(f"http://{API_HOST}/articles/{desired_article_id}").json()
+article = requests.get(f"http://{API_HOST}/articles/{desired_article["doc_id"]}").json()
 
 
-desired_material = st.selectbox("Desired material", article["materials"])
+# Use the format_func parameter to display only the names
+desired_material = st.selectbox(
+    "Desired material", article["materials"], format_func=format_option
+)
 desired_material_cost = desired_material["price"]
 
-desired_dimensions = st.selectbox("Desired dimensions", article["dimensions"])
-desired_dimensions_cost = desired_dimensions["price"]
 
-desired_finishing = st.selectbox("Desired finishing", article["finishings"])
+desired_finishing = st.selectbox(
+    "Desired finishing", article["finishings"], format_func=format_option
+)
 desired_finishing_cost = desired_finishing["price"]
 
+desired_dimensions = st.selectbox(
+    "Desired dimensions", article["dimensions"], format_func=format_option
+)
+desired_dimensions_cost = desired_dimensions["price"]
+
 st.write(f"Material cost: {desired_material_cost}")
-st.write(f"Dimensions cost: {desired_dimensions_cost}")
 st.write(f"Finishing cost: {desired_finishing_cost}")
+st.write(f"Dimensions cost: {desired_dimensions_cost}")
 
 st.write(
     f"The total cost is: {desired_material_cost + desired_dimensions_cost + desired_finishing_cost}"
 )
-
-st.json(article)
