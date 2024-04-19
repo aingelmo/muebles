e271c05552bdf4e75f0ea504ad35845b326bb2a8
Andrés Ingelmo <aingelmop@gmail.com>
2024-04-19 21:35:44 +0200

    Refactor get_all_articles and get_article_info functions in main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from fastapi import FastAPI, HTTPException
 
 from app.crud import get_document, get_unique_items
@@ -6,7 +8,8 @@ app = FastAPI()
 
 
 @app.get("/articles/all")
-async def get_all_articles():
+async def get_all_articles() -> list[dict[str, str]] | None | HTTPException:
+    """Get all articles from the Datastore."""
     try:
         return get_unique_items()
     except Exception as e:
@@ -16,7 +19,8 @@ async def get_all_articles():
 @app.get("/articles/{document_id}")
 async def get_article_info(
     document_id: str,
-):
+) -> dict[str, Any] | list[dict[str, Any]] | Any | HTTPException:
+    """Get a specific article from the Datastore."""
     try:
         document_data = get_document("articles", document_id)
         return document_data
