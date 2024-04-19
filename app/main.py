156f823ff8d41a89cd064aa82d98457cd6b0c6eb
Andrés Ingelmo <aingelmop@gmail.com>
2024-04-19 22:05:13 +0200

    Refactor get_unique_items and get_document functions in crud.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
-from typing import Any
-
 from fastapi import FastAPI, HTTPException
 
 from app.crud import get_document, get_unique_items
+from app.validation import Item, UniqueItemId
 
 app = FastAPI()
 
 
 @app.get("/articles/all")
-async def get_all_articles() -> list[dict[str, str]] | None | HTTPException:
+async def get_all_articles() -> list[UniqueItemId]:
     """Get all articles from the Datastore."""
     try:
         return get_unique_items()
@@ -17,9 +16,7 @@ async def get_all_articles() -> list[dict[str, str]] | None | HTTPException:
 
 
 @app.get("/articles/{document_id}")
-async def get_article_info(
-    document_id: str,
-) -> dict[str, Any] | list[dict[str, Any]] | Any | HTTPException:
+async def get_article_info(document_id: str) -> Item:
     """Get a specific article from the Datastore."""
     try:
         document_data = get_document("articles", document_id)
